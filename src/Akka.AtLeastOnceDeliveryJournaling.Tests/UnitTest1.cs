7ff0fa5b01fbdbb43fbae3d4854ad1054bf9383f
Aaron Stannard <aaron@petabridge.com>
2019-01-16 09:56:49 -0600

    added build system

---
+++ src/Akka.AtLeastOnceDeliveryJournaling.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Akka.AtLeastOnceDeliveryJournaling.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
