b703926863a22546a7c27f402ac3aeac1678d355
Aaron Stannard <aaron@petabridge.com>
2019-02-12 17:09:01 -0600

    added some specs to verify that we prune correctly

--- src/Akka.Persistence.Extras.Tests/DeDuplication/DeDuplicatingReceiverStateSpecs.cs
+++ src/Akka.Persistence.Extras.Tests/DeDuplication/DeDuplicatingReceiverStateSpecs.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Immutable;
+using FluentAssertions;
 using FsCheck;
 using FsCheck.Experimental;
 using FsCheck.Xunit;
@@ -16,10 +17,28 @@ namespace Akka.Persistence.Extras.Tests.DeDuplication
             return model.ToProperty();
         }
 
-        [Fact(DisplayName = "UnorderedReceiverState should prune its records correctly")]
-        public void UnorderedReceiverState_should_prune_correctly()
+        [Fact(DisplayName = "UnorderedReceiverState should prune its older senders correctly")]
+        public void UnorderedReceiverState_should_prune_older_senders_correctly()
         {
+            var timeProvider = new FakeTimeProvider(DateTime.UtcNow);
+            var receiverState = new UnorderedReceiverState(timeProvider);
+            receiverState.ConfirmProcessing(new ConfirmableMessageEnvelope(1000L, "foo", "bar"));
+            timeProvider.SetTime(TimeSpan.FromSeconds(10));
 
+            var prune = receiverState.Prune(TimeSpan.FromSeconds(5));
+            prune.prunedSenders.Should().BeEquivalentTo("foo");
+        }
+
+        [Fact(DisplayName = "UnorderedReceiverState should prune senders who are more recent than the requested prune time")]
+        public void UnorderedReceiverState_should_NOT_prune_newer_senders()
+        {
+            var timeProvider = new FakeTimeProvider(DateTime.UtcNow);
+            var receiverState = new UnorderedReceiverState(timeProvider);
+            receiverState.ConfirmProcessing(new ConfirmableMessageEnvelope(1000L, "foo", "bar"));
+            timeProvider.SetTime(TimeSpan.FromSeconds(10));
+
+            var prune = receiverState.Prune(TimeSpan.FromSeconds(15));
+            prune.prunedSenders.Should().BeEmpty();
         }
     }
 
