056839fc87b8bae89d906f7ce270f2f5a44c6913
Aaron Stannard <aaron@petabridge.com>
2019-02-12 16:07:38 -0600

    Added model-based test for receiver state

--- src/Akka.Persistence.Extras.Tests/DeDuplication/DeDuplicatingReceiverSpecs.cs
+++ src/Akka.Persistence.Extras.Tests/DeDuplication/DeDuplicatingReceiverSpecs.cs
@@ -4,27 +4,105 @@ using System.Collections.Immutable;
 using System.Linq;
 using FsCheck;
 using FsCheck.Experimental;
+using FsCheck.Xunit;
 using Xunit;
 
 namespace Akka.Persistence.Extras.Tests.DeDuplication
 {
     public class DeDuplicatingReceiverSpecs
     {
-        [Fact]
-        public void TestMethod1()
+        [Property]
+        public Property DeDuplicatingReceiverStateModel_should_hold_for_AnyOrderReceiverState()
         {
+            var model = new DeDuplicatingReceiverStateModel(new AnyOrderReceiverStateSetup());
+            return model.ToProperty();
         }
     }
 
-    public class DeDuplicatingReceiverState<TReceiverState> : Machine<TReceiverState, DeDuplicatingReceiverModelState>
-        where TReceiverState : IReceiverState
+    public class AnyOrderReceiverStateSetup : Setup<IReceiverState, DeDuplicatingReceiverModelState>
     {
-        public override Gen<Operation<TReceiverState, DeDuplicatingReceiverModelState>> Next(DeDuplicatingReceiverModelState obj0)
+        public readonly DateTime StartTime = DateTime.UtcNow;
+
+        public override IReceiverState Actual()
         {
-            throw new NotImplementedException();
+            return new UnorderedReceiverState(new FakeTimeProvider(StartTime));
         }
 
-        public override Arbitrary<Setup<TReceiverState, DeDuplicatingReceiverModelState>> Setup { get; }
+        public override DeDuplicatingReceiverModelState Model()
+        {
+            return new DeDuplicatingReceiverModelState(ImmutableDictionary<string, DateTime>.Empty, ImmutableDictionary<string, ImmutableHashSet<long>>.Empty, StartTime);
+        }
+    }
+
+    public class DeDuplicatingReceiverStateModel : Machine<IReceiverState, DeDuplicatingReceiverModelState>
+    {
+
+        public DeDuplicatingReceiverStateModel(Setup<IReceiverState, DeDuplicatingReceiverModelState> setup)
+        {
+            Setup = Arb.From(Gen.Fresh(() => setup));
+        }
+
+        public override Gen<Operation<IReceiverState, DeDuplicatingReceiverModelState>> Next(DeDuplicatingReceiverModelState obj0)
+        {
+            return Gen.OneOf(ReceiveNewMessage.Gen());
+        }
+
+        public override Arbitrary<Setup<IReceiverState, DeDuplicatingReceiverModelState>> Setup { get; }
+
+        #region StateOperations
+
+        public class ReceiveNewMessage : Operation<IReceiverState, DeDuplicatingReceiverModelState>
+        {
+            private readonly IConfirmableMessage _confirmable;
+
+            public static Gen<Operation<IReceiverState, DeDuplicatingReceiverModelState>> Gen()
+            {
+                return ConfirmableGenerators.CreateConfirmableMessage().Generator
+                    .Select(x => (Operation<IReceiverState, DeDuplicatingReceiverModelState>)new ReceiveNewMessage(x));
+            }
+
+            public ReceiveNewMessage(IConfirmableMessage confirmable)
+            {
+                _confirmable = confirmable;
+            }
+
+            public override bool Pre(DeDuplicatingReceiverModelState model)
+            {
+                return !model.AlreadyProcessed(_confirmable);
+            }
+
+            public override Property Check(IReceiverState actual, DeDuplicatingReceiverModelState model)
+            {
+                var actualHasProcessedBefore = (!actual.AlreadyProcessed(_confirmable)).ToProperty()
+                    .Label($"Should NOT have processed message [{_confirmable.SenderId}-{_confirmable.ConfirmationId}] before");
+                actual.ConfirmProcessing(_confirmable);
+                var actualHasProcessedAfter = actual.AlreadyProcessed(_confirmable).ToProperty()
+                    .Label($"Should have processed message [{_confirmable.SenderId}-{_confirmable.ConfirmationId}] after");
+
+                var lruTimesAreTheSame = (actual.TrackedSenders[_confirmable.SenderId]
+                        .Equals(model.TrackedSenders[_confirmable.SenderId]))
+                    .ToProperty()
+                    .Label(
+                        $"Actual should have same LRU time as model for sender [{_confirmable.SenderId}], but instead was " +
+                        $"(Actual={actual.TrackedSenders[_confirmable.SenderId]}, Model={model.TrackedSenders[_confirmable.SenderId]}");
+
+                return lruTimesAreTheSame.
+                    And(actualHasProcessedAfter).
+                    And(actualHasProcessedBefore);
+            }
+
+            public override DeDuplicatingReceiverModelState Run(DeDuplicatingReceiverModelState model)
+            {
+                return (DeDuplicatingReceiverModelState) model.ConfirmProcessing(_confirmable);
+            }
+
+            public override string ToString()
+            {
+                return $"{GetType()}(SenderId={_confirmable.SenderId}, ConfirmationId={_confirmable.ConfirmationId})";
+            }
+        }
+
+        #endregion
     }
 
     public class DeDuplicatingReceiverModelState : IReceiverState
