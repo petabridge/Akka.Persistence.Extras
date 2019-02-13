c1f8dc19c3f25e50df2481b3829561c01c3c8bae
Aaron Stannard <aaron@petabridge.com>
2019-02-13 14:34:20 -0600

    standardized Akka.NET version in common.props

---
+++ src/Akka.Persistence.Extras.Tests/DeDuplication/DeDuplicatingActorSpecs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.Persistence.Extras.Tests.DeDuplication
+{
+    public class DeDuplicatingActorSpecs : TestKit.Xunit2.TestKit
+    {
+    }
+}
