5527a4b157b2124e67e0094b2f1b126980712bb5
Aaron Stannard <aaron@petabridge.com>
2019-02-13 22:26:37 -0600

    added DeDuplicationReceiverActor example (#19)
    * added initial example
    
    * changed sample to use SQLite
    
    * added copyright headers

---
+++ src/Akka.Persistence.Extras.Demo.DeDuplicatingReceiver/Program.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.IO;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace Akka.Persistence.Extras.Demo.DeDuplicatingReceiver
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var config = ConfigurationFactory.ParseString(File.ReadAllText("sample.conf"));
+            using (var actorSystem = ActorSystem.Create("AtLeastOnceDeliveryDemo", config))
+            {
+                var recipientActor = actorSystem.ActorOf(Props.Create(() => new MyRecipientActor()), "receiver");
+                var atLeastOnceDeliveryActor =
+                    actorSystem.ActorOf(Props.Create(() => new MyAtLeastOnceDeliveryActor(recipientActor)),
+                        "delivery1");
+
+                var atLeastOnceDeliveryActor2 =
+                    actorSystem.ActorOf(Props.Create(() => new MyAtLeastOnceDeliveryActor(recipientActor)),
+                        "delivery2");
+
+                var atLeastOnceDeliveryActor3 =
+                    actorSystem.ActorOf(Props.Create(() => new MyAtLeastOnceDeliveryActor(recipientActor)),
+                        "delivery3");
+
+                actorSystem.WhenTerminated.Wait();
+            }
+        }
+    }
+}
