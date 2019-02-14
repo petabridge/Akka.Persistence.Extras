fbc95c2132010e82d7ba6063d06133c54c4e766d
Aaron Stannard <aaron@petabridge.com>
2019-02-13 22:05:24 -0600

    added initial example

---
+++ src/Akka.Persistence.Extras.Demo.DeDuplicatingReceiver/MyRecipientActor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Akka.Actor;
+
+namespace Akka.Persistence.Extras.Demo.DeDuplicatingReceiver
+{
+    public class MyRecipientActor : DeDuplicatingReceiveActor
+    {
+        public MyRecipientActor()
+        {
+            Command<Write>(write =>
+            {
+                Console.WriteLine("Received message {0} [id: {1}] from {2} - accept?", write.Content, CurrentConfirmationId, CurrentSenderId);
+                var response = Console.ReadLine()?.ToLowerInvariant();
+                if (!string.IsNullOrEmpty(response) && (response.Equals("yes") || response.Equals("y")))
+                {
+                    // confirm delivery only if the user explicitly agrees
+                    ConfirmAndReply(write);
+                    Console.WriteLine("Confirmed delivery of message ID {0}", CurrentConfirmationId);
+                }
+                else
+                {
+                    Console.WriteLine("Did not confirm delivery of message ID {0}", CurrentConfirmationId);
+                }
+            });
+        }
+
+        public override string PersistenceId => Context.Self.Path.Name;
+
+        protected override object CreateConfirmationReplyMessage(long confirmationId, string senderId, object originalMessage)
+        {
+            return new ReliableDeliveryAck(confirmationId);
+        }
+
+        protected override void HandleDuplicate(long confirmationId, string senderId, object duplicateMessage)
+        {
+            Console.WriteLine("Automatically de-duplicated message with ID {0} from {1}", confirmationId, senderId);
+            base.HandleDuplicate(confirmationId, senderId, duplicateMessage);
+        }
+    }
+}
---
+++ src/Akka.Persistence.Extras.Demo.DeDuplicatingReceiver/Program.cs
@@ -0,0 +1,25 @@
+﻿using Akka.Actor;
+
+namespace Akka.Persistence.Extras.Demo.DeDuplicatingReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var actorSystem = ActorSystem.Create("AtLeastOnceDeliveryDemo"))
+            {
+                var recipientActor = actorSystem.ActorOf(Props.Create(() => new MyRecipientActor()), "receiver");
+                var atLeastOnceDeliveryActor =
+                    actorSystem.ActorOf(Props.Create(() => new MyAtLeastOnceDeliveryActor(recipientActor)), "delivery1");
+
+                var atLeastOnceDeliveryActor2 =
+                    actorSystem.ActorOf(Props.Create(() => new MyAtLeastOnceDeliveryActor(recipientActor)), "delivery2");
+
+                var atLeastOnceDeliveryActor3 =
+                    actorSystem.ActorOf(Props.Create(() => new MyAtLeastOnceDeliveryActor(recipientActor)), "delivery3");
+
+                actorSystem.WhenTerminated.Wait();
+            }
+        }
+    }
+}
