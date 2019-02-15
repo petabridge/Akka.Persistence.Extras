de67cf5684a6675f0a0d8ff87ab384fcea091907
Aaron Stannard <aaron@petabridge.com>
2019-02-15 16:19:22 -0600

    created ExtraPersistence ActorSystem extension

---
+++ src/Akka.Persistence.Extras.Tests/ExtraPersistenceSpecs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Extras.Tests
+{
+    public class ExtraPersistenceSpecs : TestKit.Xunit2.TestKit
+    {
+        public ExtraPersistenceSpecs(ITestOutputHelper helper) : base(output:helper) { }
+
+        [Fact(DisplayName = "ExtraPersistence embedded HOCON should load correctly")]
+        public void Should_load_default_ExtraPersistence_HOCON()
+        {
+            ExtraPersistence.DefaultConfig().Should().NotBeNull();
+        }
+    }
+}
---
+++ src/Akka.Persistence.Extras/ExtraPersistence.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace Akka.Persistence.Extras
+{
+    /// <summary>
+    /// INTERNAL API.
+    ///
+    /// Used by the <see cref="ActorSystem"/> to create a singleton <see cref="ExtraPersistence"/>
+    /// instance specific to the actor system who calls it.
+    /// </summary>
+    public sealed class ExtraPersistenceExt : ExtensionIdProvider<ExtraPersistence>
+    {
+        public override ExtraPersistence CreateExtension(ExtendedActorSystem system)
+        {
+            return new ExtraPersistence(system);
+        }
+    }
+
+    /// <summary>
+    /// <see cref="ActorSystem"/> extension used to inject all of the relevant settings.
+    /// </summary>
+    public sealed class ExtraPersistence : IExtension
+    {
+        /// <summary>
+        /// Creates a new <see cref="ExtraPersistence"/> instance and injects all appropriate
+        /// serialization settings into <see cref="system"/>.
+        /// </summary>
+        /// <param name="system">The actor system to which this instance will be bound.</param>
+        public ExtraPersistence(ExtendedActorSystem system)
+        {
+            // forces the serialization bindings to become active
+            system.Settings.InjectTopLevelFallback(DefaultConfig());
+
+            Running = true;
+        }
+
+        /// <summary>
+        /// Used to check if all of the <see cref="ExtraPersistence"/> settings are currently active.
+        /// </summary>
+        public bool Running { get; }
+
+        /// <summary>
+        /// Returns the default HOCON configuration for Akka.Persistence.Extras.
+        /// </summary>
+        /// <returns>The built-in HOCON config.</returns>
+        internal static Config DefaultConfig()
+        {
+            return ConfigurationFactory.FromResource<ExtraPersistence>(
+                "Akka.Persistence.Extras.Config.akka.persistence.extras.conf");
+        }
+    }
+}
