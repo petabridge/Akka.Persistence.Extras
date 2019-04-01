6a3fc3c974f7e31213f6db4379388d532b497b01
Aaron Stannard <aaron@petabridge.com>
2019-04-01 16:30:50 -0500

    PersistenceSupervisorActor (#31)
    * working on adding PersistenceSupervisor
    
    * added End2End integration spec for PersistentSupervisor

---
+++ src/Akka.Persistence.Extras.Tests/Supervision/AkkaPersistenceSupervisionEnd2EndSpecs.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Akka.Event;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Persistence.Extras.Supervision;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Extras.Tests.Supervision
+{
+    /// <summary>
+    /// Persistent actor that is going to try to get its work done using the <see cref="FailingJournal"/>
+    /// </summary>
+    public class WorkingPersistentActor : ReceivePersistentActor
+    {
+        public class AddToCount : IConfirmableMessage
+        {
+            public AddToCount(long confirmationId, string senderId, int countValue)
+            {
+                ConfirmationId = confirmationId;
+                SenderId = senderId;
+                CountValue = countValue;
+            }
+
+            public long ConfirmationId { get; }
+            public string SenderId { get; }
+
+            public int CountValue { get; }
+        }
+
+        public class GetCount
+        {
+            public static readonly GetCount Instance = new GetCount();
+            private GetCount() { }
+        }
+
+        private readonly ILoggingAdapter _log = Context.GetLogger();
+        private int _currentCount = 0;
+
+        public WorkingPersistentActor(string persistenceId)
+        {
+            PersistenceId = persistenceId;
+
+            Recover<int>(i =>
+            {
+                _log.Info("Recovery: Adding [{0}] to current count [{1}] - new count: [{2}]", i, _currentCount, _currentCount + i);
+                _currentCount += i;
+            });
+
+            Recover<SnapshotOffer>(o =>
+            {
+                if (o.Snapshot is int i)
+                {
+                    _log.Info("Recovery: Setting initial count to [{1}]", i);
+                    _currentCount = i;
+                }
+            });
+
+            Command<AddToCount>(e =>
+            {
+                Persist(e.CountValue, iN =>
+                {
+                    _log.Info("Command: Adding [{0}] to current count [{1}] - new count: [{2}]", iN, _currentCount, _currentCount + iN);
+                    _currentCount += iN;
+
+                        // ACK the message back to parent
+                        Context.Parent.Tell(new Confirmation(e.ConfirmationId, PersistenceId));
+                });
+            });
+
+            Command<GetCount>(g =>
+            {
+                Sender.Tell(_currentCount);
+            });
+        }
+
+        public override string PersistenceId { get; }
+    }
+
+    public class AkkaPersistenceSupervisionEnd2EndSpecs : TestKit.Xunit2.TestKit
+    {
+        public static readonly Config Config = @"akka.persistence.journal.plugin = ""akka.persistence.journal.failure""
+                           akka.persistence.journal.failure.class = """ + typeof(FailingJournal).AssemblyQualifiedName + "\"";
+
+        public AkkaPersistenceSupervisionEnd2EndSpecs(ITestOutputHelper output)
+            : base(Config, output: output) { }
+
+        [Fact(DisplayName = "End2End: PersistenceSupervisor should ensure delivery of all events")]
+        public void PersistenceSupervisor_should_ensure_delivery_of_all_events()
+        {
+            var childProps = Props.Create(() => new WorkingPersistentActor("fuber"));
+            var supervisor = PersistenceSupervisor.PropsFor((o, l) =>
+            {
+                if (o is int i)
+                {
+                    return new WorkingPersistentActor.AddToCount(l, string.Empty, i);
+                }
+
+                return new ConfirmableMessageEnvelope(l, string.Empty, o);
+            }, o => o is int, childProps, "myActor", strategy:SupervisorStrategy.StoppingStrategy.WithMaxNrOfRetries(100));
+
+            var sup = Sys.ActorOf(supervisor, "fuber");
+            Watch(sup);
+
+            foreach (var i in Enumerable.Range(1, 100))
+            {
+                sup.Tell(i);
+            }
+
+            Within(TimeSpan.FromMinutes(2), () =>
+            {
+                AwaitAssert(() =>
+                {
+                    sup.Tell(WorkingPersistentActor.GetCount.Instance);
+                    ExpectMsg(5050, TimeSpan.FromMilliseconds(25)); // sum should be 5051
+                }, interval: TimeSpan.FromMilliseconds(40));
+            });
+        }
+    }
+}
---
+++ src/Akka.Persistence.Extras.Tests/Supervision/PersistenceSupervisorSpecs.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Akka.Actor;
+using Akka.Pattern;
+using Akka.Persistence.Extras.Supervision;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+using static Akka.Persistence.Extras.Tests.Supervision.PersistenceSupervisorHelpers;
+
+namespace Akka.Persistence.Extras.Tests.Supervision
+{
+    public class PersistenceSupervisorSpecs : TestKit.Xunit2.TestKit
+    {
+        public PersistenceSupervisorSpecs(ITestOutputHelper helper) : base(output: helper)
+        {
+
+        }
+
+        [Fact(DisplayName = "PersistenceSupervisor should forward messages to child")]
+        public void PersistenceSupervisor_should_forward_normal_msgs()
+        {
+            var childProps = Props.Create(() => new AckActor(TestActor, "fuber", true));
+            var supervisorProps = PersistenceSupervisorFor(o => o is string, childProps, "myPersistentActor");
+            var actor = Sys.ActorOf(supervisorProps);
+
+            actor.Tell(BackoffSupervisor.GetCurrentChild.Instance);
+            var child = ExpectMsg<BackoffSupervisor.CurrentChild>().Ref;
+            Watch(child);
+
+            // sanity check to ensure that the two actors are different
+            child.Equals(actor).Should().BeFalse();
+
+            // send a non-persisted message
+            actor.Tell(1);
+            ExpectMsg(1);
+
+            // send a persisted message
+            actor.Tell("string");
+            ExpectMsg<Confirmation>().ConfirmationId.Should().Be(1L);
+            actor.Tell("string1");
+            ExpectMsg<Confirmation>().ConfirmationId.Should().Be(2L);
+
+            // shutdown the parent
+            actor.Tell(PoisonPill.Instance);
+            ExpectTerminated(child);
+        }
+
+        [Fact(DisplayName = "PersistenceSupervisor should buffer and re-deliver un-ACKed events for child after restart")]
+        public void PersistenceSupervisor_should_buffer_unAcked_messages()
+        {
+            var childProps = Props.Create(() => new AckActor(TestActor, "fuber", true));
+            var supervisorProps = PersistenceSupervisorFor(o => o is string, childProps, "myPersistentActor");
+            var actor = Sys.ActorOf(supervisorProps);
+
+            actor.Tell(BackoffSupervisor.GetCurrentChild.Instance);
+            var child = ExpectMsg<BackoffSupervisor.CurrentChild>().Ref;
+            Watch(child);
+
+            // disable ACK-ing - will be turned on automatically via restart
+            actor.Tell(AckActor.ToggleAck.Instance);
+
+            // send a non-persisted message
+            actor.Tell(1);
+            ExpectMsg(1);
+
+            // send some events
+            actor.Tell("event1");
+            actor.Tell("event2");
+            ExpectNoMsg(200); // no replies back
+
+            EventFilter.Exception<ApplicationException>("boom").ExpectOne(() =>
+            {
+                actor.Tell(AckActor.Fail.Instance);
+                ExpectTerminated(child); // child should be stopped by default parent supervisor strategy
+            });
+
+            ExpectMsg<Confirmation>().ConfirmationId.Should().Be(1L);
+            ExpectMsg<Confirmation>().ConfirmationId.Should().Be(2L);
+        }
+
+        [Fact(DisplayName = "PersistenceSupervisor should buffer messages while child is restarting")]
+        public void PersistenceSupervisor_should_buffer_messages_while_ActorIsRestarting()
+        {
+            var childProps = Props.Create(() => new AckActor(TestActor, "fuber", true));
+
+            // make the backoff window huge, so we have to manually restart it
+            var supervisorConfig = new PersistenceSupervisionConfig(o => o is string, ToConfirmableMessage,
+                new ManualReset(), TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(10));
+
+            var supervisorProps = Props.Create(() =>
+                new PersistenceSupervisor(childProps, "myPersistentActor", supervisorConfig, null));
+            var actor = Sys.ActorOf(supervisorProps);
+
+            actor.Tell(BackoffSupervisor.GetCurrentChild.Instance);
+            var child = ExpectMsg<BackoffSupervisor.CurrentChild>().Ref;
+            Watch(child);
+
+            // send some events
+            actor.Tell("event1");
+            actor.Tell("event2");
+            ExpectMsg<Confirmation>().ConfirmationId.Should().Be(1L);
+            ExpectMsg<Confirmation>().ConfirmationId.Should().Be(2L);
+
+            // kill the child
+            EventFilter.Exception<ApplicationException>("boom").ExpectOne(() =>
+            {
+                actor.Tell(AckActor.Fail.Instance);
+                ExpectTerminated(child); // child should be stopped by default parent supervisor strategy
+            });
+
+            // send a blend of events and other messages prior to restart
+            actor.Tell("event3");
+            actor.Tell(1);
+            actor.Tell(2);
+            actor.Tell("event4");
+            actor.Tell(true);
+
+            // shouldn't hear anything back
+            ExpectNoMsg(250);
+
+            // create the child manually (timer is still running in background)
+            actor.Tell(BackoffSupervisor.StartChild.Instance);
+
+            ExpectMsg<Confirmation>().ConfirmationId.Should().Be(3L);
+            ExpectMsg(1);
+            ExpectMsg(2);
+            ExpectMsg<Confirmation>().ConfirmationId.Should().Be(4L);
+            ExpectMsg(true);
+        }
+
+        [Fact(DisplayName = "PersistentSupervisor should kill itself if child RestartCount exceeded")]
+        public void PersistentSupervisor_should_kill_child_and_self_if_RestartCount_Exceeded()
+        {
+            var childProps = Props.Create(() => new AckActor(TestActor, "fuber", true));
+            var supervisorConfig = new PersistenceSupervisionConfig(o => o is string, ToConfirmableMessage,
+                new ManualReset(), TimeSpan.FromMilliseconds(1), TimeSpan.FromMilliseconds(10));
+
+            var supervisorProps = Props.Create(() =>
+                new PersistenceSupervisor(childProps, "myPersistentActor", supervisorConfig, SupervisorStrategy.StoppingStrategy.WithMaxNrOfRetries(1)));
+            var actor = Sys.ActorOf(supervisorProps);
+
+            Watch(actor);
+
+            actor.Tell(BackoffSupervisor.GetCurrentChild.Instance);
+            var child = ExpectMsg<BackoffSupervisor.CurrentChild>().Ref;
+            Watch(child);
+
+            actor.Tell(AckActor.Fail.Instance); // forces the actor to fail
+            ExpectTerminated(child);
+            
+            AwaitCondition(() =>
+            {
+                actor.Tell(BackoffSupervisor.GetCurrentChild.Instance);
+                child = ExpectMsg<BackoffSupervisor.CurrentChild>().Ref;
+                if (child.IsNobody())
+                    return false;
+
+                Watch(child);
+                actor.Tell(AckActor.Fail.Instance); // forces the actor to fail
+                ExpectTerminated(child);
+                return true;
+            });
+
+            ExpectTerminated(actor);
+        }
+    }
+}
