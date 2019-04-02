4ab667952ec8f1f8fcfe02c28721d380585bcf3c
Aaron Stannard <aaron@petabridge.com>
2019-04-02 14:34:31 -0500

    added stand-alone PersistenceSupervisor sample (#36)

---
+++ src/Akka.Persistence.Extras.Demo.PersistenceSupervisor/Program.cs
@@ -0,0 +1,43 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Linq;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace Akka.Persistence.Extras.Demo.PersistenceSupervisor
+{
+    internal class Program
+    {
+        public static readonly Config Config = @"akka.persistence.journal.plugin = ""akka.persistence.journal.failure""
+                           akka.persistence.journal.failure.recovery-event-timeout = 2s
+                           akka.persistence.journal.failure.class = """ +
+                                               typeof(FailingJournal).AssemblyQualifiedName + "\"";
+
+        private static void Main(string[] args)
+        {
+            var actorSystem = ActorSystem.Create("MyActorSystem", Config);
+
+            var childProps = Props.Create(() => new WorkingPersistentActor("fuber"));
+            var supervisor = Extras.PersistenceSupervisor.PropsFor((o, l) =>
+                {
+                    if (o is int i)
+                        return new WorkingPersistentActor.AddToCount(l, string.Empty, i);
+
+                    return new ConfirmableMessageEnvelope(l, string.Empty, o);
+                }, o => o is int, childProps, "myActor",
+                strategy: SupervisorStrategy.StoppingStrategy.WithMaxNrOfRetries(100));
+
+            var sup = actorSystem.ActorOf(supervisor, "fuber");
+
+            // final sum should be 5050
+            foreach (var i in Enumerable.Range(1, 100))
+                sup.Tell(i);
+
+            actorSystem.WhenTerminated.Wait();
+        }
+    }
+}
---
+++ src/Akka.Persistence.Extras.Demo.PersistenceSupervisor/WorkingPersistentActor.cs
@@ -0,0 +1,82 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="WorkingPersistentActor.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Actor;
+using Akka.Event;
+
+namespace Akka.Persistence.Extras.Demo.PersistenceSupervisor
+{
+    /// <summary>
+    ///     Persistent actor that is going to try to get its work done using the <see cref="FailingJournal" />
+    /// </summary>
+    public class WorkingPersistentActor : ReceivePersistentActor
+    {
+        private readonly ILoggingAdapter _log = Context.GetLogger();
+        private int _currentCount;
+
+        public WorkingPersistentActor(string persistenceId)
+        {
+            PersistenceId = persistenceId;
+
+            Recover<int>(i =>
+            {
+                _log.Info("Recovery: Adding [{0}] to current count [{1}] - new count: [{2}]", i, _currentCount,
+                    _currentCount + i);
+                _currentCount += i;
+            });
+
+            Recover<SnapshotOffer>(o =>
+            {
+                if (o.Snapshot is int i)
+                {
+                    _log.Info("Recovery: Setting initial count to [{1}]", i);
+                    _currentCount = i;
+                }
+            });
+
+            Command<AddToCount>(e =>
+            {
+                Persist(e.CountValue, iN =>
+                {
+                    _log.Info("Command: Adding [{0}] to current count [{1}] - new count: [{2}]", iN, _currentCount,
+                        _currentCount + iN);
+                    _currentCount += iN;
+
+                    // ACK the message back to parent
+                    Context.Parent.Tell(new Confirmation(e.ConfirmationId, PersistenceId));
+                });
+            });
+
+            Command<GetCount>(g => { Sender.Tell(_currentCount); });
+        }
+
+        public override string PersistenceId { get; }
+
+        public class AddToCount : IConfirmableMessage
+        {
+            public AddToCount(long confirmationId, string senderId, int countValue)
+            {
+                ConfirmationId = confirmationId;
+                SenderId = senderId;
+                CountValue = countValue;
+            }
+
+            public int CountValue { get; }
+
+            public long ConfirmationId { get; }
+            public string SenderId { get; }
+        }
+
+        public class GetCount
+        {
+            public static readonly GetCount Instance = new GetCount();
+
+            private GetCount()
+            {
+            }
+        }
+    }
+}
