d0f71bc009f5dee5ab1672ec67cc3563e2783aa9
IgorFedchenko <igor@fedchenko.pro>
2020-07-14 16:41:14 +0300

    Added code for ticket https://help.petabridge.com/a/tickets/134?

---
+++ Deserializer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Persistence.Extras;
+using Akka.Persistence.Serialization;
+using Newtonsoft.Json;
+
+namespace Deserializer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Read snapshot bytes
+            var bytes = await File.ReadAllBytesAsync("0068_B_.BIN");
+            
+            // Get configured ActorSystem
+            var persistenceConfig = ConfigurationFactory.FromResource<ExtraPersistence>("Akka.Persistence.Extras.Config.akka.persistence.extras.conf");
+            var system = ActorSystem.Create("Deserializer", persistenceConfig);
+            
+            // Deserialize snapshot
+            var persistenceMessageSerializer = new PersistenceSnapshotSerializer(system as ExtendedActorSystem);
+            var snapshot = persistenceMessageSerializer.FromBinary<Snapshot>(bytes);
+            
+            // Print snapshot to the output
+            Console.Write(JsonConvert.SerializeObject(snapshot.Data));
+        }
+    }
+}
