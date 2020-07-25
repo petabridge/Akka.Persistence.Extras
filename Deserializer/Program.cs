0567ecb91db5de9521aee92003c2af728a6c1b43
IgorFedchenko <igor@fedchenko.pro>
2020-07-25 17:54:51 +0300

    Updated deserializer program

--- Deserializer/Program.cs
+++ Deserializer/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Configuration;
 using Akka.Persistence.Extras;
 using Akka.Persistence.Serialization;
+using Akka.Persistence.Serialization.Proto.Msg;
 using Newtonsoft.Json;
 
 namespace Deserializer
@@ -14,8 +16,8 @@ namespace Deserializer
         static async Task Main(string[] args)
         {
             // Read snapshot bytes
-            var bytes = await File.ReadAllBytesAsync("0068_B_.BIN");
-            
+            var bytes = await File.ReadAllBytesAsync("Snapshot.bin");
+
             // Get configured ActorSystem
             var persistenceConfig = ConfigurationFactory.FromResource<ExtraPersistence>("Akka.Persistence.Extras.Config.akka.persistence.extras.conf");
             var system = ActorSystem.Create("Deserializer", persistenceConfig);
@@ -26,6 +28,18 @@ namespace Deserializer
             
             // Print snapshot to the output
             Console.Write(JsonConvert.SerializeObject(snapshot.Data));
+            
+            /*PersistentPayload payload = PersistentPayload.Parser.ParseFrom(bytes);
+
+            string manifest = "";
+            if (payload.PayloadManifest != null) manifest = payload.PayloadManifest.ToStringUtf8();
+
+            var data = system.Serialization.Deserialize(payload.Payload.ToByteArray(), 501, "");
+            
+            // Print snapshot to the output
+            Console.Write(JsonConvert.SerializeObject(data));*/
         }
+        
+        
     }
 }
